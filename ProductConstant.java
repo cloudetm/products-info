1e7e4fa73342d9db3548c69f56630197d888b716
cloudetm <anil.karnal@gmail.com>
2019-10-17 21:56:08 +0200

    Add files via upload

---
+++ ProductConstant.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.telefonica.product.constant;
+
+/**
+ * @author anil
+ *
+ */
+public class ProductConstant {
+	
+	public static final String START ="start";
+	public static final String END ="end";
+	public static final String PRODUCTCONTROLLER ="productController";
+	public static final String IPRODUCTDAOMAPPER ="IProductDAOMapper";
+	public static final String GETDATA ="getProductData";
+	public static final String GETALLDATA ="getAllProductData";
+	public static final String DELETEDATA ="deleteData";
+	public static final String ADDDATA ="addProduct";
+	public static final String TIMETAKEN ="TimeTaken";
+	public static final String REMOVEPRODUCT ="removeProduct";
+	public static final String PRODUCTMANAGER ="ProductManager";
+	public static final String CREATETABLE = "createProductTable";
+}
